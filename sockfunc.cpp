9e40cb2d87d650d68f80462dc866ec8611251995
DacianKing <iloveplaystation2000@gmail.com>
2016-07-05 14:00:09 -0400

    Create sockfunc.cpp

---
+++ sockfunc.cpp
@@ -0,0 +1,31 @@
+#include <windows.h>
+#include <winsock2.h>
+
+
+char *SendGetRequest() {
+
+
+
+}
+
+
+char *SendPostRequest() {
+
+
+
+
+}
+
+
+
+char *ResolveDomainName() {
+
+
+
+}
+
+
+char SendUDPResponse() {
+
+
+}
